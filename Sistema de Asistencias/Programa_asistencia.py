e5d9a123d7bc60b613a188a6d14cf3f3903aead7
MaguiiCobos <103389203+MaguiiCobos@users.noreply.github.com>
2024-10-29 18:09:45 -0300

    Cámara en tiempo real

--- Sistema de Asistencias/Programa_asistencia.py
+++ Sistema de Asistencias/Programa_asistencia.py
@@ -1,48 +1,36 @@
 import cv2
 import face_recognition as fr
 import os
-import numpy
+import numpy as np
 from datetime import datetime
 import tkinter as tk
 from tkinter import simpledialog, messagebox
 
-# Crear la ventana principal de tkinter (aunque no se mostrará)
+# Crear la ventana principal de tkinter
 root = tk.Tk()
 root.withdraw()  # Oculta la ventana principal
-
 
 # Crear base de datos
 ruta = 'Inscriptos'
 mis_imagenes = []
-nombres_empleados = []
-lista_empleados = os.listdir(ruta)
+nombres_inscriptos = []
+lista_inscriptos = os.listdir(ruta)
 
-for nombre in lista_empleados:
-
+# Carga los nombres de los inscriptos 
+for nombre in lista_inscriptos:
     imagen_actual = cv2.imread(f"{ruta}/{nombre}")
     mis_imagenes.append(imagen_actual)
-    nombres_empleados.append(os.path.splitext(nombre)[0])
+    nombres_inscriptos.append(os.path.splitext(nombre)[0])
 
-
-print(nombres_empleados)
+print(f"Lista de inscriptos para el evento: \n{nombres_inscriptos}")
 
 # Codificar imagenes
 def codificar(imagenes):
-
-    # Crear una lista nueva
     lista_codificada = []
-
-    # Pasar todas las imagenes a RGB
     for imagen in imagenes:
         imagen = cv2.cvtColor(imagen, cv2.COLOR_BGR2RGB)
-
-        # Codificar
         codificado = fr.face_encodings(imagen)[0]
-
-        # Agregar a la lista
         lista_codificada.append(codificado)
-
-    # Devolver lista codificada
     return lista_codificada
 
 # Registrar los ingresos
@@ -60,92 +48,77 @@ def registrar_ingresos(persona):
         str_ahora = ahora.strftime('%H:%M:%S')
         f.writelines(f"\n{persona}, {str_ahora}")
 
-
-#funcion agregada
+# Funciones agregada
 def entregar_cronograma():
     f = open("cronograma.csv", "r+")
     lista_datos = f.readlines()
-    print(lista_datos[1:])
+    messagebox.showinfo("Cronograma de la Reunión", lista_datos[1:])
 
+# Registra un nuevo usuario
 def registro(imagen):
-    nombre_inscripto = input("Ingrese su nombre y apellido: ")
-    ruta = f"Inscriptos/{nombre_inscripto}.jpg"
-    cv2.imwrite(ruta, imagen)
-    print("Te has registrado con exito")
-
-def validar_imagenes():
-    # Tomar una imagen de camara web
-    captura = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-    # Leer imagen de la camara
-    exito, imagen = captura.read()
-    if not exito:
-        print("No se ha podido tomar la captura")
+    nombre_inscripto = simpledialog.askstring("Registro", "Ingrese su nombre y apellido:")
+    if nombre_inscripto:
+        ruta = f"Inscriptos/{nombre_inscripto}.jpg"
+        cv2.imwrite(ruta, imagen)
+        messagebox.showinfo("Registro", "Te has registrado con éxito")
     else:
+        messagebox.showinfo("Registro", "Por favor ingrese su nombre y apellido")
+        registro(imagen)
 
-        # Reconocer cara en captura
-        cara_captura = fr.face_locations(imagen)
+# Abre la cámara y muestra video en tiempo real
+def abrir_camara():
+    captura = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+    while True:
+        exito, frame = captura.read()
+        cv2.imshow("Presiona 'c' para capturar la imagen", frame)
+        if cv2.waitKey(1) & 0xFF == ord('c'):
+            captura.release()
+            cv2.destroyAllWindows()
+            return frame
 
-        # Codificar cara capturada
-        cara_captura_codificada = fr.face_encodings(imagen, cara_captura)
+# Validar la imagen de usuario
+def validar_imagenes():
+    imagen = abrir_camara()    
+    cara_captura = fr.face_locations(imagen)
+    cara_captura_codificada = fr.face_encodings(imagen, cara_captura)
+    
+    if not cara_captura_codificada:
+        messagebox.showerror("Error", "No se ha detectado ninguna cara en la imagen")
+        validar_imagenes()
+        return
 
-        # Buscar coincidencias | Doble loop
-        for cara_codif, cara_ubic in zip(cara_captura_codificada, cara_captura):
-            coincidencias = fr.compare_faces(lista_empleados_codificada, cara_codif)
-            distancias = fr.face_distance(lista_empleados_codificada, cara_codif)
-
-            print(distancias)
-
-            indice_coincidencia = numpy.argmin(distancias)
-
-            # Mostrar si existen coincidencias
-            if distancias[indice_coincidencia] > 0.6:
-                opcion = simpledialog.askstring("Inscribirse", "No estas en la lista de inscriptos\nDeseas registarte? (S/N): ")
-                if opcion:
-                    opcion = opcion.strip().upper()
-                    if opcion == "S":
-                        registro(imagen)
-                    else:
-                        print("Nos vimos")
+    for cara_codif, cara_ubic in zip(cara_captura_codificada, cara_captura):
+        distancias = fr.face_distance(lista_inscriptos_codificada, cara_codif)
+        indice_coincidencia = np.argmin(distancias)
+        
+        if distancias[indice_coincidencia] > 0.6:
+            opcion = messagebox.askyesno("Inscripción", "No estás en la lista de inscritos. ¿Deseas registrarte?")
+            if opcion:  # click en Sí
+                registro(imagen)
             else:
+                messagebox.showinfo("Registro", "Operación cancelada")
+        else:
+            print(distancias[indice_coincidencia])
+            nombre = nombres_inscriptos[indice_coincidencia]
+            entregar_cronograma()
+            y1, x2, y2, x1 = cara_ubic
+            cv2.rectangle(imagen, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.rectangle(imagen, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
+            cv2.putText(imagen, nombre, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+            registrar_ingresos(nombre)
+            messagebox.showinfo("Acceso Permitido", f"Bienvenid@, {nombre}. Puedes ingresar a la reunión")
+            cv2.imshow("Imagen capturada", imagen)
+            cv2.waitKey(4000)  # Mostrar por 4 segundos
+            cv2.destroyAllWindows()
 
-                # Buscar el nombre del empleado encontrado
-                nombre = nombres_empleados[indice_coincidencia]
-                print(f"Bienvenid@, {nombre}, puedes ingresar a la reunion")
-
-                #Agregamos funcion para dar el cronograma de la reunion
-                entregar_cronograma()
-
-                y1, x2, y2, x1 = cara_ubic
-
-                cv2.rectangle(imagen, (x1, y1), (x2, y2), (0, 255, 0), 2)
-                cv2.rectangle(imagen, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
-                cv2.putText(imagen, nombre, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
-
-                registrar_ingresos(nombre)
-
-                # Mostrar la imagen obtenida
-                cv2.imshow("Imagen web", imagen)
-
-                # Mantener ventana abierta
-                #cv2.waitKey(0)
-
-
-lista_empleados_codificada = codificar(mis_imagenes)
+lista_inscriptos_codificada = codificar(mis_imagenes)
 
 # Mensaje de bienvenida
-messagebox.showinfo("Registro de Asistencia", "Bienvenid@ al sistema de registro de asistencia de Invictus")
-# Solicitar al usuario si desea registrarse
-opcion = simpledialog.askstring("Registro", "¿Quieres registrarte? (S/N):")
+messagebox.showinfo("Registro de Asistencia", "Bienvenid@ al sistema de registro de asistencias de evento")
+opcion = messagebox.askyesno("Registro", "¿Desea ingresar al evento?")
 
-if opcion:
-    opcion = opcion.strip().upper()
-    if opcion == "S":
-        print("Validaremos tu imagen para saber si estas registrado")
-        validar_imagenes()
-        
-    elif opcion == "N":
-        print("Has elegido no registrarte.")
-    else:
-        print("Opcion no valida. Por favor, elige 'S' o 'N'.")
+if opcion:  # Si el usuario hace clic en "Sí"
+    messagebox.showinfo("Validación", "Abriendo la cámara para capturar tu imagen")
+    validar_imagenes()
 else:
-    messagebox.showwarning("Advertencia", "No se ha ingresado ninguna opción.")
+    messagebox.showinfo("Registro", "Has elegido no registrarte.")
